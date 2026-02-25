7eece18cf61b28ea83d5ad999227caab7613cb94
GARRISON-2 <garyarner10@gmail.com>
2026-02-25 15:24:53 -0700

    save before switch to tifffile

--- run_quint.py
+++ run_quint.py
@@ -1,47 +1,97 @@
+#import PyNutil 
 from pathlib import Path
 import subprocess
 from PIL import Image
 import gc
 
-PROJ_ROOT = Path(__file__).parent
 
-in_dir = PROJ_ROOT / 'local_data/test_folder/empty_tiffs'
-mask_dir = PROJ_ROOT
+Image.MAX_IMAGE_PIXELS = None
+
+# grab current working directory
+PROJ_ROOT = Path.cwd()
+
+dir24 = "/Xenium/TBI 24hrs"
+dir48 = "/Xenium/TBI 48hrs"
+dir48_MH2 = "/Xenium/TBI 48hrs + MH2"
+
+png_output_dir =  PROJ_ROOT / "local_data" / "dapi_run_1" / "pngs"
+
+mask_dir = PROJ_ROOT 
 atlas_dir = PROJ_ROOT
 out_dir = PROJ_ROOT
 
+# # Grabbing input folders/files
+# folder_list = []
+
+# # set which image file type to search for in the directory
+# file_type = "*.tif"
+
+# for folder in folder_list:
+
+#     # grab all files of file_type in chosen input directory
+#     path_list = in_dir.rglob(file_type)
+
+
+# test on specific file
+path_list = [Path()]
+# check if big TIFF
+with open(path_list[0], "rb") as f:
+    print(f.read(4))
+
+img = Image.open(path_list[0])
+img.load()
 
 class IMG_PACK():
     def __init__(self, image_path):
-        in_path = image_path 
-        png_path = None
+        self.in_path = image_path
+        self.png_path = None
 
-    def makePNGCopy(self, png_width = 1500, png_height=1000):
+    def makePNGCopy(self, output_dir, png_width = 1500, png_height=1000):
         # create png path name
-        png_path = self.in_path.with_name(self.in_path.stem + "_ds.png")
+        png_path = Path(str(output_dir) + self.in_path.stem + '_dwnscl')
+
+        print(self.in_path)
 
         # open input image using Pillow
-        img = Image.open(self.in_path)
+        with Image.open(self.in_path) as img:
 
-        # resize image to desired ratio
-        ds_img = img.resize((png_width, png_height))
+            # resize image to desired ratio
+            ds_img = img.resize((png_width, png_height))
 
-        # convert to rgb and then save png copy of the input image. the original image remains unchanged
-        ds_img.convert("RGB").save(png_path)
-        self.png_path = png_path
+            # convert to rgb and then save png copy of the input image. the original image remains unchanged
+            ds_img.convert("RGB").save(png_path)
+            self.png_path = png_path
+        
+
+    def getPNGSize(self):
+          # open input image using Pillow
+        try:
+            with Image.open(self.png_path) as png:
+
+                return png.size
+
+        except Exception as e:
+            return (f"ERROR - Unable to open image. {e}")
+        
 
 
-# set which image file type to search for in the directory
-file_type = "*.tif"
+check_img = True
 
-# grab all files of file_type in chosen input directory
-path_list = list(in_dir.rglob(file_type))
-
+IP_list = []
 
 # loop through all input image files from glob list
 for path in path_list:
-    # make image pack to cleanly hold paths for current file
+#     # make image pack to cleanly hold paths for current file
     IP = IMG_PACK(path)
 
-    # create downscaled png copy
-    IP.makePNGCopy()
+#     # create downscaled png copy
+    IP.makePNGCopy(output_dir=png_output_dir)
+
+    if check_img:
+        with Image.open(IP.png_path) as png:
+            print(f"New png path: {IP.png_path}. png size: {IP.getPNGSize()}")
+
+    IP_list.append(IP)
+
+
+
